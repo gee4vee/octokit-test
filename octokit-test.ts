d56c1733f44bc6688bfe111c48a22536b8654266
Gabriel Valencia <gvalenc@us.ibm.com>
2023-01-30 12:16:53 -0600

    rewrite as typescript
    Signed-off-by: Gabriel Valencia <gvalenc@us.ibm.com>

---
+++ octokit-test.ts
@@ -0,0 +1,48 @@
+// Replace me with `require` if you're using CommonJS modules
+import { Octokit } from "octokit";
+
+const octokitOptions: {
+  owner: string;
+  repo: string;
+  path: string;
+} & {
+  ref?: string | undefined;
+} & {
+  mediaType: {
+    format: string;
+  };
+} = {
+  owner: "gvalenc",
+  repo: "environment-aio-iks-app-us",
+  path: "IBM-ResourceGroup/provider_metadata.yaml",
+  mediaType: {
+    format: "raw",
+  },
+};
+
+const authToken = process.env.GH_TOKEN;
+const baseUrl = `http://github.ibm.com/api/v3`;
+
+let octokit;
+if (authToken) {
+  octokit = new Octokit({
+    baseUrl,
+    auth: authToken,
+    log: console,
+  });
+} else {
+  octokit = new Octokit({
+    baseUrl,
+    log: console,
+  });
+}
+
+// github.ibm.com/gvalenc/environment-aio-iks-app-us/IBM-ResourceGroup
+(async () => {
+  const response = await octokit.request(
+    "GET /repos/{owner}/{repo}/contents/{path}",
+    octokitOptions
+  );
+
+  console.log(response);
+})();
